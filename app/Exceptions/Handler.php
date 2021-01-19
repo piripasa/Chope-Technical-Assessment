787faab7c4be11a921cd5187d208efe4cde50212
Zaman-A-Piri Pasa <zaman-a-piripasa@Zaman-A-Piris-MacBook-Pro>
2021-01-19 16:24:31 +0600

    Handle various exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,11 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\ApiController;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -28,8 +30,9 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
      * @return void
+     * @throws Exception
      */
     public function report(Exception $exception)
     {
@@ -39,12 +42,76 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Exception $exception
      * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     * @throws \ReflectionException
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        //return parent::render($request, $exception);
+
+        $exceptionClass = (new \ReflectionClass($exception))->getShortName();
+        $message = "Something went wrong on server";
+        $debugMode = config('app.debug', false);
+
+        switch ($exceptionClass) {
+            case 'HttpResponseException':
+                $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+                break;
+
+            case 'MethodNotAllowedHttpException':
+                $status = Response::HTTP_METHOD_NOT_ALLOWED;
+                $message = "Route has been used with wrong method";
+                break;
+
+            case 'ModelNotFoundException':
+                $status = Response::HTTP_METHOD_NOT_ALLOWED;
+                $message = $debugMode ? $exception->getMessage() : "No result found";
+                break;
+
+            case 'NotFoundHttpException':
+                $status = Response::HTTP_NOT_FOUND;
+                $message = "Requested route has not found";
+                break;
+
+            case 'AuthorizationException':
+                $status = Response::HTTP_FORBIDDEN;
+                $message = $exception->getMessage() ?? "Wrong or invalid parameter provided";
+                break;
+
+            case 'ValidatorException':
+                $status = Response::HTTP_BAD_REQUEST;
+                $message = "Wrong or invalid parameter provided";
+                break;
+
+            case 'InvalidArgumentException':
+            case 'InvalidException':
+                $status = Response::HTTP_BAD_REQUEST;
+                $message = $exception->getMessage();
+                break;
+
+            case 'QueryException':
+                $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $message = $debugMode ? $exception->getMessage() : $message;
+                break;
+
+            case 'AuthException':
+                $status = Response::HTTP_UNAUTHORIZED;
+                $message = $exception->getMessage();
+                break;
+
+            case 'ValidationException':
+                $status = Response::HTTP_UNPROCESSABLE_ENTITY;
+                $message = json_decode($exception->getResponse()->getContent(), 1);
+                break;
+
+            default:
+                $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $message = $debugMode ? $exception->getMessage() : $message;
+                break;
+        }
+
+        return app(ApiController::class)->sendError($exceptionClass, (array)$message, $status);
     }
 }
