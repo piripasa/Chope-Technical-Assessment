ceeec7f32e5f317a6a18af3c576745ade1048c26
Zaman-A-Piri Pasa <zaman-a-piripasa@Zaman-A-Piris-MacBook-Pro>
2021-01-20 09:01:29 +0600

    Add logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,24 +4,32 @@
 namespace App\Http\Controllers;
 
 
+use App\Events\UserLoginEvent;
+use App\Events\UserLogoutEvent;
+use App\Events\UserRegisterEvent;
 use App\Exceptions\AuthException;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Repository\UserRepository;
 use App\Service\JwtToken;
+use App\Service\Transformer\UserTransformer;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class AuthController extends ApiController
 {
     private $repository;
+    private $transformer;
 
     /**
      * AuthController constructor.
      * @param UserRepository $repository
+     * @param UserTransformer $transformer
      */
-    public function __construct(UserRepository $repository)
+    public function __construct(UserRepository $repository, UserTransformer $transformer)
     {
         $this->repository = $repository;
+        $this->transformer = $transformer;
     }
 
     /**
@@ -32,11 +40,12 @@ class AuthController extends ApiController
     public function register(RegisterRequest $request)
     {
         try {
-            $this->repository->store([
+            $user = $this->repository->store([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => app('hash')->make($request->password)
             ]);
+            event(new UserRegisterEvent($user));
             return $this->sendResponse([], 'Registered successfully', 201);
         } catch (\Exception $exception) {
             throw $exception;
@@ -52,22 +61,37 @@ class AuthController extends ApiController
     {
         if ($user = $this->repository->findBy('email', $request->username)) {
             if (app('hash')->check($request->password, $user->password)) {
-                $expiration = 24 * 60 * 60;
-                $token = (new JwtToken([config('jwt.algo')], config('jwt.secret'), 'chope', $expiration))->createToken($user, app('redisCache'));
+                $expiration = config('jwt.ttl');
+                $token = (new JwtToken(
+                    [config('jwt.algo')],
+                    config('jwt.secret'),
+                    config('jwt.issuer'),
+                    $expiration)
+                )->createToken($user['id']);
+                app('redisCache')->set(sprintf('user:%d:token', $user->id), $token, $expiration);
+                app('redisCache')->set($token, serialize($user), $expiration);
+                event(new UserLoginEvent($user));
                 return $this->sendResponse([
                     "token_type" => "Bearer",
                     "expires_in" => $expiration,
                     'access_token' => $token,
-                    'user' => [
-                        'id' => $user->id,
-                        'name' => $user->name,
-                        'email' => $user->email,
-                    ]
+                    'user' => $this->transformer->transform($user)
                 ], 'Login successfully');
             }
         }
 
         throw new AuthException("These credentials do not match our records.");
+    }
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function logout(Request $request)
+    {
+        app('redisCache')->delete($request->bearerToken());
+        app('redisCache')->delete(sprintf('user:%d:token', $request->user()->id));
+        event(new UserLogoutEvent($request->user()));
+        return $this->sendResponse([], 'Logout successfully');
     }
 }
